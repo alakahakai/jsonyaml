ac9b4222668f974e81bd621e51cc25ecd5c252f7
Ray Qiu <ray.qiu@gmail.com>
2014-12-07 18:56:46 -0800

    init commit

---
+++ src/json2yaml.hs
@@ -0,0 +1,27 @@
+import           Control.Applicative        ((<$>))
+import           Control.Monad              (forM_)
+import           Data.Aeson                 (eitherDecode)
+import qualified Data.ByteString            as B
+import qualified Data.ByteString.Lazy.Char8 as BLC
+import qualified Data.Yaml                  as Y
+import           System.Environment         (getArgs)
+import           System.Exit                (exitFailure)
+
+helpMessage :: IO ()
+helpMessage = putStrLn "Usage: json2yaml <FILE> [<FILE> ..]\nFor stdin input, use - as FILE" >> exitFailure
+
+toYAML :: Show a => Either a Y.Value -> IO B.ByteString
+toYAML x = case x of
+  Left err -> print err >> exitFailure
+  Right v -> return $ Y.encode (v :: Y.Value)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> helpMessage
+    _ ->
+      forM_ args $ \arg ->
+        case arg of
+          "-" -> B.getContents >>= toYAML . Y.decodeEither' >>= B.putStr
+          _ -> eitherDecode <$> BLC.readFile arg >>= toYAML >>= B.writeFile (arg ++ ".yaml")
---
+++ src/yaml2json.hs
@@ -0,0 +1,27 @@
+import           Data.Aeson (Value)
+import           Data.Aeson.Encode.Pretty (encodePretty)
+import           System.Environment (getArgs)
+import           Control.Monad (forM_)
+import           System.Exit (exitFailure)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy.Char8 as BLC
+import qualified Data.Yaml as Y
+
+helpMessage :: IO ()
+helpMessage = putStrLn "Usage: yaml2json <FILE> [<FILE> ..]\nFor stdin input, use - as FILE" >> exitFailure
+
+toJSON :: Show a => Either a Value -> IO BLC.ByteString
+toJSON x = case x of
+  Left err -> print err >> exitFailure
+  Right v -> return $ encodePretty (v :: Value)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> helpMessage
+    _ ->
+      forM_ args $ \arg ->
+        case arg of
+          "-" -> B.getContents >>= toJSON . Y.decodeEither' >>= BLC.putStr
+          _ -> Y.decodeFileEither arg >>= toJSON >>= BLC.writeFile (arg ++ ".json")
